12aa7943136b8026799723fcd077a13253b3e82b
nitinskumavat <nitin.s.kumavat@gmail.com>
2019-04-14 16:02:07 +0530

    Updates

---
+++ precache-manifest.4b21d062ebab0804148ae952c8ed1f96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "df82856f76ae02bc7da9d97b5561a51f",
+    "url": "/static/media/cert.df82856f.png"
+  },
+  {
+    "revision": "dc846958103a70a61b94860bd2b704c3",
+    "url": "/static/media/b3.dc846958.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cdd9ffbe5f820b316a18",
+    "url": "/static/js/main.c54a09b9.chunk.js"
+  },
+  {
+    "revision": "f9f9780ec7d029e95476",
+    "url": "/static/js/2.403336e3.chunk.js"
+  },
+  {
+    "revision": "cdd9ffbe5f820b316a18",
+    "url": "/static/css/main.97c566b3.chunk.css"
+  },
+  {
+    "revision": "f9f9780ec7d029e95476",
+    "url": "/static/css/2.ea90f412.chunk.css"
+  },
+  {
+    "revision": "f61a6c041057e69407b5cc0179701116",
+    "url": "/index.html"
+  }
+];
